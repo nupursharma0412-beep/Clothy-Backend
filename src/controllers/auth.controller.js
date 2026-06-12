2dc34b21bae6463cd6f5a66e18ae513539f471f0
Jay Singh <jaysinghrajput265@gmail.com>
2026-06-13 00:57:49 +0530

    Front end

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -3,12 +3,12 @@ import userModel from "../models/user.model.js";
 import jwt from 'jsonwebtoken'
 import { config } from "../config/config.js";
 
-async function sendTokenResponse(user , res,message) {
-    
+async function sendTokenResponse(user, res, message) {
+
     const token = jwt.sign({
-        id:user._id
-    },config.JWT_SECRET,{
-        expiresIn:"7d"
+        id: user._id
+    }, config.JWT_SECRET, {
+        expiresIn: "7d"
     })
 
     res.cookie("token", token, {
@@ -19,108 +19,108 @@ async function sendTokenResponse(user , res,message) {
     })
     res.status(200).json({
         message,
-        success:true,
-        user:{
-            id:user._id,
-            email:user.email,
-            contact:user.contact,
-            fullname:user.fullname,
-            role:user.role
+        success: true,
+        user: {
+            id: user._id,
+            email: user.email,
+            contact: user.contact,
+            fullname: user.fullname,
+            role: user.role
         }
     })
 }
-  
-
-export const register = async (req,res)=>{
-        const {email , password , contact , fullname, isSeller} = req.body;
 
 
-        try {
-            const existingUser = await  userModel.findOne({
-                $or:[
-                    {email},
-                    {contact}
-                ]
-            })
+export const register = async (req, res) => {
+    const { email, password, contact, fullname, isSeller } = req.body;
 
-            if(existingUser) {
-                return res.status(400).json({
-                    message:"user with this email or contact already exists"
-                })
-            }
-
-            const user = await userModel.create({
-                email,
-                contact,
-                password,
-                fullname,
-                role :isSeller? "seller":"buyer"
-            })
-
-            await sendTokenResponse(user ,  res, "user register successfully")
-
-
-        } catch (error) {
-            return res.status(500).json({message:"server error"})
-        }
-}
-
-export const login = async (req,res)=>{
-
-
-    const {email , password} = req.body;
 
     try {
-        const user = await userModel.findOne({email})
-        if(!user){
+        const existingUser = await userModel.findOne({
+            $or: [
+                { email },
+                { contact }
+            ]
+        })
+
+        if (existingUser) {
             return res.status(400).json({
-                message:"Invalid email or password"
+                message: "user with this email or contact already exists"
+            })
+        }
+
+        const user = await userModel.create({
+            email,
+            contact,
+            password,
+            fullname,
+            role: isSeller ? "seller" : "buyer"
+        })
+
+        await sendTokenResponse(user, res, "user register successfully")
+
+
+    } catch (error) {
+        return res.status(500).json({ message: "server error" })
+    }
+}
+
+export const login = async (req, res) => {
+
+
+    const { email, password } = req.body;
+
+    try {
+        const user = await userModel.findOne({ email })
+        if (!user) {
+            return res.status(400).json({
+                message: "Invalid email or password"
             })
         }
 
         const isMatch = await user.comparePassword(password)
-        if(!isMatch){
+        if (!isMatch) {
             return res.status(400).json({
-                message:"Invalid email or password"
+                message: "Invalid email or password"
             })
         }
-        await sendTokenResponse(user , res , "User login successfully")
-        
+        await sendTokenResponse(user, res, "User login successfully")
+
     } catch (error) {
         console.log(error);
-        
-        return res.status(500).json({message:"server error"})
+
+        return res.status(500).json({ message: "server error" })
     }
 }
 
 
-export const googleCallback = async (req,res)=>{
-    const { id ,displayName , emails , photos} = req.user
-      
+export const googleCallback = async (req, res) => {
+    const { id, displayName, emails, photos } = req.user
+
     const email = emails[0].value;
     const profilePic = photos[0].value
 
-    let user = await userModel.findOne({email})
+    let user = await userModel.findOne({ email })
 
-    if(!user){
+    if (!user) {
         user = await userModel.create({
-           email ,
-           fullname:displayName,
-           googleId:id,
+            email,
+            fullname: displayName,
+            googleId: id,
         })
     }
 
     const token = jwt.sign({
-        id:user._id,
+        id: user._id,
 
-    },config.JWT_SECRET,{
-        expiresIn:'7d'
+    }, config.JWT_SECRET, {
+        expiresIn: '7d'
     })
 
     res.cookie("token", token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
-        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+        secure: true,
+        sameSite: "none",
         maxAge: 7 * 24 * 60 * 60 * 1000
     })
     res.redirect("https://clothy-frontend-mu.vercel.app/")
@@ -128,17 +128,17 @@ export const googleCallback = async (req,res)=>{
 
 
 
-export const getMe = async(req,res)=>{
+export const getMe = async (req, res) => {
     const user = req.user;
     return res.status(200).json({
-        message:"user fatch successfully",
-        success:true,
-        user:{
-            id:user._id,
-            email:user.email,
-            contact:user.contact,
-            fullname:user.fullname,
-            role:user.role
+        message: "user fatch successfully",
+        success: true,
+        user: {
+            id: user._id,
+            email: user.email,
+            contact: user.contact,
+            fullname: user.fullname,
+            role: user.role
         }
     })
 }
