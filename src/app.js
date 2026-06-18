9b293550889b64e881b0701b7ad06d97283784c6
Jay Singh <jaysinghrajput265@gmail.com>
2026-06-18 21:15:31 +0530

    Updated project

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 app.use(cors({
-  origin: "https://clothy-frontend-five.vercel.app",
+  origin: "https://clothy-frontend-mu.vercel.app",
   methods: ["GET", "POST", "PUT", "DELETE","PATCH"],
   credentials: true
 }))
